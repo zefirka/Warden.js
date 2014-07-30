9ab00fe5da526814e324909e4cf95684ab5472cf
Dat Mkrtchyan <d.mkrtchyan@parcsis.org>
2014-07-30 11:29:12 +0400

    Added databus prototype

--- src/plugins/getStreamOf.js
+++ src/plugins/getStreamOf.js
@@ -1,4 +1,4 @@
-(function() {
+(function(){
   (function(root, factory) {
     var Warden;
     if (typeof exports === 'object' && exports) {
@@ -31,6 +31,151 @@
     }
   
   });
-}).call(this);
 
 
+  Warden.makeStream = function(x, context){
+    var stream;
+    if(typeof x === 'function'){
+      var type = [0,0,0,0,0].map(function(item, i){ return (((Math.random() * Math.pow(10, i+2))  >> 0 ) >> (Math.abs(i-1)))}).join("-"),
+          stream = new Stream(type, context);
+        debugger;
+      x(function(expectedData){
+        stream.eval(expectedData);
+      });
+
+    }else
+    if(typeof x === 'string'){
+      stream = new Stream(x, context);
+    }else{
+      throw "Unexpected data type at stream\n";
+    }
+
+    return stream;
+  }
+
+
+  function Stream(dataType, context, toolkit){
+    var listeningBuses = [];
+    this.drive = []; 
+
+    var bus = new DataBus();
+    bus.setHost(this);
+
+    this.eval = function(data){
+      console.log("Evaluated");
+      listeningBuses.forEach(function(bus){
+        bus.fire(data, context);
+      });
+    }
+
+    this.push = function(bus){
+      listeningBuses.push(bus);
+    }
+
+    this.get = function(){
+      return bus;
+    }
+
+    return this;
+  }
+
+  function DataBus(proc){
+    
+    //private keys
+    var processor = proc,
+        host = 0;
+
+    this._publicData = {
+      taken : 0,
+      filtered : 0,
+      skipped : 0,
+      fired : 0
+    };
+
+    this.setHost = function(nhost){
+      host = nhost;
+    };
+
+    this.getHost = function(nhost){
+      return host;
+    };
+
+    this.getProcessor = function(){
+      return processor;
+    }
+
+    this.addProcess = function(process){
+      processor.push(process);
+    }
+
+    this.fire = function(event, context){
+      var self = this;
+
+      if(processor){
+        while(processor.next(event, context, false)){
+          event = processor.result;
+        }
+      }
+
+      return this.callback.apply(context, [event]);;
+    }
+  }
+
+  DataBus.prototype.listen = function(x){
+    this.getHost().push(this);
+    this.callback = x;
+    var nb = this.clone();
+    return nb;
+  }
+
+  DataBus.prototype.clone = function() {
+    var nbus = new DataBus(this.getProcessor());
+    nbus.setHost(this.getHost());
+    return nbus;
+  }
+
+  DataBus.prototype.map = function(x) {
+    var fn;
+
+    if(typeof x === 'string'){
+      fn = function(event, context){
+        if(exists(event[x])){
+          return event[x]
+        }else{
+          return x
+        }
+      }
+    }else{
+      fn = function(event, context){
+        event = x.apply(context, [event]);
+        return event;
+      }
+    }
+
+    var nbus = this.clone();
+    nbus.addProcess(fn);
+    return nbus;
+  };
+
+
+
+  function Processor(){
+    var processes = [], i = 0;
+    this.result = null;
+
+    this.tick = function(event, context, preventValue) {
+      var res = processes[i].apply(context, [event]);
+      if(i<processes.length || (preventValue !== undefined && res === preventValue)){
+        res = false;
+      }
+      i++;
+      return res;
+    };
+
+    this.push = function(process){
+      processes.push(process)
+    }
+    
+  }
+
+})();
