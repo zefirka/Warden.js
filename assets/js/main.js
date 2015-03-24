43ef0af5c001aff828986ae7de24a930e84b610b
Dat Mkrtchyan <d.mkrtchyan@parcsis.org>
2015-03-24 18:58:47 +0400

    Fixed css

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,13 +3,21 @@ $('pre code').each(function(i, block) {
 	hljs.highlightBlock(block);
 });
 
+function fn_active(el){
+	if(location.pathname.split('/').pop() == el.find('a').attr('href')){
+		el.addClass('active');
+	}else{
+		el.removeClass('active');
+	}
+}
+
 $(function(){
 	$("#submenu li").each(function(){
-		if(location.pathname.split('/').pop() == $(this).find('a').attr('href')){
-			$(this).addClass('active');
-		}else{
-			$(this).removeClass('active');
-		}
+		fn_active($(this));
+	});
+
+	$("nav li").each(function(){
+		fn_active($(this));
 	});
 });
 
