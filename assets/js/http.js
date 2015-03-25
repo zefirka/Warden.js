df4810566399a7f7884f14d7d58cec3b212263c9
Dat Mkrtchyan <d.mkrtchyan@parcsis.org>
2015-03-25 11:41:49 +0400

    Fixed http

--- assets/js/http.js
+++ assets/js/http.js
@@ -3,7 +3,24 @@ var http = {};
 var gets = Warden.Stream(function(trigger){
   this.get = function(url){
     $.get(url, trigger)
+      .fail(function(err){
+        trigger(err);
+      });
   }
 }, http);
 
 http.get('templates/test/file.html'); //this file exist
+
+function isString(res){
+  return typeof res == 'string';
+}
+
+var successes = gets.filter(isString),
+    errors = gets.filter(Warden.Utils.not(isString));
+
+var errorMessage = "<p class='error'> Error: {{status}}: {{statusText}}</p>";
+
+errors
+  .inerpolate(errorMessage)
+  .merger(successes)
+  .bindTo($(".box"), 'html');
