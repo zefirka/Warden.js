7df5e3e30ff15694711f8218647368172051323f
Dat Mkrtchyan <d.mkrtchyan@parcsis.org>
2015-04-16 15:24:38 +0400

    Compiled

--- assets/js/formula.js
+++ assets/js/formula.js
@@ -2,21 +2,21 @@ $(function function_name (argument) {
    	var from = function(src, type, f){
    		return src.stream(type);
    	}
-   	var value = function(id, typeMap){
+   	var value = function(id, type, typeMap,){
    		var str, src = $("#" + id);
    		if(typeMap){
-   			str = from(src, 'keyup').map("@val()").map(typeMap);
+   			str = from(src, type).map("@val()").map(typeMap);
    		}else{
-   			str = from(src, 'keyup').map("@val()");
+   			str = from(src, type.map("@val()");
    		}
    		str.fire((typeMap ? typeMap(src.val()) : src.val()), src);
    		return str;
    	}
 
-   	var a = value('a', parseInt),
-		b = value('b', parseInt),
-		c = value('c', parseInt),
-		d = value('d', parseInt);
+   	var a = value('a', 'keyup', parseInt),
+		b = value('b', 'keyup', parseInt),
+		c = value('c', 'keyup', parseInt),
+		d = value('d', 'keyup', parseInt);
 		
 	var res = Warden.Formula([a,b,c,d], function(a,b,c,d){
 		return (a + b + c) * d;
@@ -26,9 +26,9 @@ $(function function_name (argument) {
 
 
 
-	var color = value("color"),
-    	shape = value("shape"),
-    	size = value("size");
+	var color = value("color", 'change'),
+    	shape = value("shape", 'change'),
+    	size = value("size", 'change');
 
 	var figure = Warden.Formula([color,size,shape], function(color,size,shape){
 		return "Figure now is " + size + (shape ? " " : " and ") + color  + " " + shape;
